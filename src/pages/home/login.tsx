47d92c73b1afdf24408650443905bbbf6f2ce3b2
gongjian <15031617367@163.com>
2022-11-03 20:21:20 +0800

    登录注册表单创建 表单验证完成

--- src/pages/home/login.tsx
+++ src/pages/home/login.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import { Button, Form, Input } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+const LoginContainer = styled.div`
+  width: 600px;
+  margin: 20px auto;
+`;
+const TitleContainer = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+const ButtonContainer = styled(Button)`
+  width: 100%;
+`;
+interface FormLabel {
+  username: String;
+  passwrod: String;
+}
 const Login: React.FC<any> = () => {
+  const onFinish = (value: FormLabel) => {
+    console.log('success', value);
+  };
   return (
-    <div>
-      <h1>this is page Login</h1>
-    </div>
+    <LoginContainer>
+      <TitleContainer>登 录</TitleContainer>
+      <Form name="normal_login" className="login-form" onFinish={onFinish}>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: '请输入用户名' }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="用户名"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: '请输入密码' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="密码"
+          />
+        </Form.Item>
+        <Form.Item>
+          <ButtonContainer
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            登 录
+          </ButtonContainer>
+        </Form.Item>
+      </Form>
+    </LoginContainer>
   );
 };
 export default Login;
--- src/pages/home/register.tsx
+++ src/pages/home/register.tsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { createRef } from 'react';
+import styled from 'styled-components';
+import { Button, Form, Input } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+const LoginContainer = styled.div`
+  width: 600px;
+  margin: 20px auto;
+`;
+const TitleContainer = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+const ButtonContainer = styled(Button)`
+  width: 100%;
+`;
+interface FormLabel {
+  username: String;
+  passwrod: String;
+}
 const Register: React.FC<any> = () => {
+  const onFinish = (value: FormLabel) => {
+    console.log('success', value);
+  };
   return (
-    <div>
-      <h2>this is Register</h2>
-    </div>
+    <LoginContainer>
+      <TitleContainer>注 册</TitleContainer>
+      <Form name="normal_login" className="login-form" onFinish={onFinish}>
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: '请输入用户名' }]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="用户名"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: '请输入密码' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="密码"
+          />
+        </Form.Item>
+        <Form.Item
+          name="repeatPassword"
+          rules={[
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject('两次密码输入不一致');
+              },
+            }),
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="请重复输入密码"
+          />
+        </Form.Item>
+        <Form.Item>
+          <ButtonContainer
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            注 册
+          </ButtonContainer>
+        </Form.Item>
+      </Form>
+    </LoginContainer>
   );
 };
 export default Register;
